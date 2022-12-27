e63b34c3947d4f6ae27ac97527db3f11fdaa4a21
iam-AbdulBasit <120900763+iam-AbdulBasit@users.noreply.github.com>
2022-12-27 22:53:13 +0500

    Create Trffic signle.py

---
+++ Trffic signle.py
@@ -0,0 +1,9 @@
+Traffic_light = input("Enter light")
+if Traffic_light=='Red':
+    print("Car has to stop")
+elif Traffic_light=='Yellow':
+    print("Car has to wait")
+elif Traffic_light=='Green':
+    print("Car is allowed to go")
+else:
+    print("unrecognized single")            
