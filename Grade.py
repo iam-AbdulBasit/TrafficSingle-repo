24725ce73a5a5a5a678ed33b01a9a80a064dd5dc
iam-AbdulBasit <120900763+iam-AbdulBasit@users.noreply.github.com>
2023-01-05 00:12:50 +0500

    Create Grade.py
    Students grade

---
+++ Grade.py
@@ -0,0 +1,11 @@
+number = int(input("enter number"))
+if number >= 80:
+    print("your grde is A")
+elif number >= 70:
+    print("your grade is B")
+elif number >= 60:
+    print("your grade is C")
+elif number >= 50:
+    print("your grade is D")
+else:
+    print("you are fail need to hard work")
